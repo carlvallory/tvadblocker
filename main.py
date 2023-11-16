4c8c567040fdd169faea1fe6f2c3fe86a0a9e1c2
cvallory <carlvallory@gmail.com>
2023-11-16 16:18:40 -0300

    Update main.py

--- main.py
+++ main.py
@@ -126,6 +126,8 @@ class TVAdBlocker:
                     self.ws.call(requests.SetCurrentScene(self.tv_scene))
                 else:
                     try:
+                        #pytesseract.pytesseract.tesseract_cmd = r'C:\Users\USER\AppData\Local\Tesseract-OCR\tesseract.exe'
+                        
                         path = self.path
                         fileName = self.fileName
                         filePath = os.path.join(path, f"{fileName}")
